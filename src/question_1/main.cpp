7b8699cab2c120db3c989c3139ad55b3e1ab336f
Lilj2335 <jeffrey.rhodan@g.austincc.edu>
2024-10-17 19:41:20 +0000

    Question 1 Done

--- src/question_1/main.cpp
+++ src/question_1/main.cpp
@@ -1,6 +1,16 @@
+#include "question1.h"
+#include <iostream>
+
+using namespace std;
 
 int main()
 {
+    cout<<"Please input seconds since 1970: ";
+
+    int input;   
+    cin>>input;
+
+    cout<<get_hours(input)<<":"<<get_minutes(input)<<":"<<get_seconds(input)<<"\n";
 
     return 0;
 }
--- test/question_test_1/question_tests_1.cpp
+++ test/question_test_1/question_tests_1.cpp
@@ -10,3 +10,13 @@ TEST_CASE("test")
 {
 	REQUIRE(test_config() == true);
 }
+
+TEST_CASE("Verify Question 1", "Question 1 verification") {
+	REQUIRE(get_hours(1570846218) == 2);
+	REQUIRE(get_minutes(1570846218) == 10);
+	REQUIRE(get_seconds(1570846218) == 18);
+
+	REQUIRE(get_hours(1570875018) == 10);
+	REQUIRE(get_minutes(1570875018) == 10);
+	REQUIRE(get_seconds(1570875018) == 18);
+}
